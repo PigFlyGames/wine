88e11c42751221b37709ec0463caaad307d501d1
Dorek Biglari <dbiglari@gmail.com>
2014-08-23 17:57:53 -0500

    wined3d: Added sm4 DCL_OUTPUT opcode.

--- dlls/wined3d/glsl_shader.c
+++ dlls/wined3d/glsl_shader.c
@@ -6674,6 +6674,7 @@ static const SHADER_HANDLER shader_glsl_instruction_handler_table[WINED3DSIH_TAB
     /* WINED3DSIH_DCL_VERTICES_OUT      */ shader_glsl_nop,
     /* WINED3DSIH_DCL_INPUT             */ shader_glsl_nop,
     /* WINED3DSIH_DCL_INPUT_PS          */ shader_glsl_nop,
+    /* WINED3DSIH_DCL_OUTPUT            */ shader_glsl_nop,
     /* WINED3DSIH_DEF                   */ shader_glsl_nop,
     /* WINED3DSIH_DEFB                  */ shader_glsl_nop,
     /* WINED3DSIH_DEFI                  */ shader_glsl_nop,
